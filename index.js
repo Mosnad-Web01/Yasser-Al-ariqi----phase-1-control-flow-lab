cf2db525f430b232d2a7f0147b156ed9505c1b06
Yasser Esam <yaaser10esam1010@gmail.com>
2024-08-14 09:56:51 +0300

    adding js code

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let response = "";
+  if (distance <= 199) {
+    return (response = "This one is on me!");
+  } else if (distance >= 400 && distance <= 2000) {
+    return (response = "That will be twenty bucks.");
+  } else if (distance > 2000 && distance < 2500) {
+    return (response = "I will gladly take your thirty bucks.");
+  } else {
+    return (response = "No can do.");
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  return destination == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+
+  }
 }
